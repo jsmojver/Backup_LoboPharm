124c776b04d29f4a659cd7a6ab6467ab98e2bf38
jsmojver <josip.smojver@gmail.com>
2021-03-25 14:53:41 +0100

    initial commit2

---
+++ project/settings_local.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+ProjectPath = os.path.realpath(os.path.dirname(__file__))
+DataRoot = os.path.normpath(ProjectPath + '/../data/') + '/'
+StaticRoot = os.path.normpath(ProjectPath + '/../static/') + '/'
+LocalePaths = (os.path.normpath(ProjectPath + '/locale/') + '/',)
+FontPath = os.path.normpath(StaticRoot + 'fonts/') + '/'
+sys.path.append(os.path.normpath(ProjectPath) + '/')
+
+#*************************************************************
+# Environment settings
+
+Deploy = False
+DebugToolbar = False
+PrependWWW = False
+
+
+Admins = (
+    ('Eden Kirin', 'edkirin@gmail.com'),
+)
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'diocles',                      # Or path to database file if using sqlite3.
+        'USER': 'diocles',                      # Not used with sqlite3.
+        'PASSWORD': 'pharmaphalanx44c01',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    },
+    'abda': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'abda',                      # Or path to database file if using sqlite3.
+        'USER': 'diocles',                      # Not used with sqlite3.
+        'PASSWORD': 'pharmaphalanx44c01',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    },
+    'eskulap': {
+        'ENGINE': 'django.db.backends.oracle', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '192.168.1.229/ORCL',                      # Or path to database file if using sqlite3.
+        'USER': 'IDEMA',                      # Not used with sqlite3.
+        'PASSWORD': 'IDEMA-ESKULAP',                  # Not used with sqlite3.
+        'HOST': '192.168.1.229',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+DATABASE_ROUTERS = [ 'project.eskulap.router.Router' ]
+
+UseCaching = True
+if UseCaching:
+    Caches = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+            'KEY_PREFIX': 'diocles',
+        }
+    }
+else:
+    Caches = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
+    }
+
+EMAIL_HOST = 'mail.t-com.hr'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = "noreply@lobofarm.hr"  # from address for server error emails
+SERVER_EMAIL = "noreply@lobofarm.hr"  # used as the from address for django.core.mail.mail_admins() and django.core.mail.mail_managers()
+
+VIRMAN_PDF = False
